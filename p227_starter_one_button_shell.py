9f30a50be4f5967f9778675a2269cf4e6203e56d
Kareena Zheng <kareena.zheng@crpusd.org>
2026-01-14 09:32:07 -0800

    Pretty colors

--- p227_starter_one_button_shell.py
+++ p227_starter_one_button_shell.py
@@ -41,9 +41,32 @@ def mSave():
   
   file.write(text_to_save)
   file.close()
-    
+
+# Main window
+
 root = tk.Tk()
+root.wm_geometry("1000x600")
+root.configure(bg = "LightCyan2")
 frame = tk.Frame(root)
+frame.pack()
+
+# creates the frame with label for the input text box
+frame_URL = tk.Frame(root, pady=10,  bg="LightCyan2") # change frame color
+frame_URL.pack()
+
+# decorative label
+url_label = tk.Label(frame_URL, text="Enter a URL of interest: ", 
+    compound="center",
+    font=("comic sans", 14),
+    bd=0, 
+    relief=tk.FLAT, 
+    fg="black",
+    bg="LightCyan2")
+url_label.pack(side=tk.LEFT)
+url_entry= tk.Entry(frame_URL,  font=("comic sans", 14)) # change font
+url_entry.pack(side=tk.LEFT)
+
+frame = tk.Frame(root,  bg="LightCyan2") # change frame color
 frame.pack()
 
 # set up button to run the do_command function
@@ -79,25 +102,15 @@ nslookup_btn = tk.Button(frame, text="Retrieve DNS data",
     bg="thistle2", activebackground="thistle4")
 nslookup_btn.pack() 
 
-# creates the frame with label for the input text box
-frame_URL = tk.Frame(root, pady=10,  bg="black") # change frame color
-frame_URL.pack()
-
-# decorative label
-url_label = tk.Label(frame_URL, text="Enter a URL of interest: ", 
+# nmap button
+nmap_btn = tk.Button(frame, text="Find live hosts, devices, and connections", 
+    command=lambda:do_command("nmap"),
     compound="center",
-    font=("comic sans", 14),
+    font=("comic sans", 12),
     bd=0, 
-    relief=tk.FLAT, 
-    cursor="heart",
-    fg="mediumpurple3",
-    bg="black")
-url_label.pack(side=tk.LEFT)
-url_entry= tk.Entry(frame_URL,  font=("comic sans", 14)) # change font
-url_entry.pack(side=tk.LEFT)
-
-frame = tk.Frame(root,  bg="black") # change frame color
-frame.pack()
+    relief="flat",
+    bg="thistle2", activebackground="thistle4")
+nmap_btn.pack() 
 
 # Adds an output box to GUI.
 command_textbox = tksc.ScrolledText(frame, height=10, width=100)
