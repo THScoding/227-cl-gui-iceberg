c4363a0a41ff96cc22d558637ca4a8ea4178d19a
Kareena Zheng <kareena.zheng@crpusd.org>
2026-01-15 10:16:32 -0800

    Radiobuttons added

--- p227_starter_one_button_shell.py
+++ p227_starter_one_button_shell.py
@@ -28,7 +28,21 @@ def do_command(command):
 
 # Save function.
 def mSave(progressbar, start_button):
-  filename = asksaveasfilename(defaultextension='.txt',filetypes = (('Text files', '*.txt'),('Python files', '*.py *.pyw'),('All files', '*.*')))
+  
+  # get file type (checked by radiobuttons)
+  selected_type = file_type_variable.get()
+  
+  if selected_type == ".txt":
+    filetypes = [("Text Files", "*.txt"), ("All files", "*.*")]
+    defaultextension = ".txt"
+  elif selected_type == ".py":
+    filetypes = [("Python File", ".py"), ("All files", "*.*")]
+    defaultextension = ".py"
+  elif (selected_type == "*.*"):
+    filetypes = [("All Files", "*.*")]
+    defaultextension = "*.*"
+    
+  filename = asksaveasfilename(defaultextension = defaultextension, filetypes = filetypes)
   if filename is None:
     return
   file = open (filename, mode = 'w')
@@ -65,14 +79,14 @@ url_entry= tk.Entry(frame_URL,  font=("comic sans", 14)) # change font
 url_entry.pack(side=tk.LEFT, pady = 20)
 
 # input frame
-frame = tk.Frame(root,  bg="LightCyan2") # change frame color
-frame.pack()
+input_frame = tk.Frame(root,  bg="LightCyan2") # change frame color
+input_frame.pack()
 
 # set up button to run the do_command function
 # Makes the command button pass it's name to a function using lambda
 
 # ping button
-ping_btn = tk.Button(frame, text="Check if a URL is up and active", 
+ping_btn = tk.Button(input_frame, text="Check if a URL is up and active", 
     command=lambda:do_command("ping"),
     compound="center",
     font=("comic sans", 12),
@@ -82,7 +96,7 @@ ping_btn = tk.Button(frame, text="Check if a URL is up and active",
 ping_btn.pack(side = tk.LEFT, padx = 20) 
 
 # tracert button
-tracert_btn = tk.Button(frame, text="Map how data packets travel", 
+tracert_btn = tk.Button(input_frame, text="Map how data packets travel", 
     command=lambda:do_command("tracert"),
     compound="center",
     font=("comic sans", 12),
@@ -92,7 +106,7 @@ tracert_btn = tk.Button(frame, text="Map how data packets travel",
 tracert_btn.pack(side = tk.LEFT, padx = 20) 
 
 # nslookup button
-nslookup_btn = tk.Button(frame, text="Retrieve DNS data", 
+nslookup_btn = tk.Button(input_frame, text="Retrieve DNS data", 
     command=lambda:do_command("nslookup"),
     compound="center",
     font=("comic sans", 12),
@@ -102,7 +116,7 @@ nslookup_btn = tk.Button(frame, text="Retrieve DNS data",
 nslookup_btn.pack(side = tk.LEFT, padx = 20) 
 
 # ipconfig button
-ipconfig_btn = tk.Button(frame, text="Display IP network configuration", 
+ipconfig_btn = tk.Button(input_frame, text="Display IP network configuration", 
     command=lambda:do_command("ipconfig"),
     compound="center",
     font=("comic sans", 12),
@@ -112,14 +126,29 @@ ipconfig_btn = tk.Button(frame, text="Display IP network configuration",
 ipconfig_btn.pack(side = tk.LEFT, padx = 20) 
 
 # output frame
-frame = tk.Frame(root,  bg="LightCyan2") # change frame color
-frame.pack()
+output_frame = tk.Frame(root,  bg="LightCyan2") # change frame color
+output_frame.pack()
 
 # Adds an output box to GUI.
-command_textbox = tksc.ScrolledText(frame, height=10, width=100)
+command_textbox = tksc.ScrolledText(output_frame, height=10, width=100)
 command_textbox.pack(pady = 30)
 
-# ----- Progress Bar ----
+# ----- Radiobuttons -----
+
+# default file type to .txt
+file_type_variable = tk.StringVar(value = ".txt")
+
+# Creates radiobuttons
+txt_button = tk.Radiobutton(output_frame, text = "Text (.txt)", variable = file_type_variable, value = ".txt")
+txt_button.pack()
+
+py_button = tk.Radiobutton(output_frame, text = "Python (.py)", variable = file_type_variable, value = ".py")
+py_button.pack()
+
+all_button = tk.Radiobutton(output_frame, text = "All Files (*.*)", variable = file_type_variable, value = "*.*")
+all_button.pack()
+
+# ----- Progress Bar -----
 
 def start_task(progressbar, start_button):
     """Simulates a task and updates the progress bar."""
